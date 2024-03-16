db1869612b61d9e11ee7c7ce51e4021f88337938
ParsaMohajeri <parsamohajeri1188@gmail.com>
2024-03-16 23:35:46 +0330

    .

--- django_course/setting/prod.py
+++ django_course/setting/prod.py
@@ -34,3 +34,30 @@ STATICFILES_DIRS = [
 # CSRF_COOKIE_SECURE=True
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
+
+
+
+
+
+
+#__________________________________________________________
+# این هارو توی سایتم نوشتم
+# SECURE_BROWSER_XSS_FILTER = True
+# CSRF_COOKIE_SECURE=True
+# X_FRAME_OPTIONS = 'SAMEORIGINE'
+
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# ## Strict-Transport-Security
+# SECURE_HSTS_SECONDS = 15768000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+
+# ## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+# SECURE_SSL_REDIRECT = True 
+
+# # for more security
+# CSRF_COOKIE_SECURE = True
+# CSRF_USE_SESSIONS = True
+# CSRF_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SAMESITE = 'Strict'
